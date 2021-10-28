23ebfe53c7877d4abd29bcdb24d2436e6812cdc4
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-10-27 22:17:33 -0300

    Foi feito o tratamento de NullPointerException. Se o array passado for nullo ele vai cai dentro do meu catch

--- src/main/java/algoritmo/Carro.java
+++ src/main/java/algoritmo/Carro.java
@@ -5,18 +5,26 @@ import static algoritmo.Produto.buscaMenor;
 public class Carro {
     public static void main(String[] args) {
 
-        Produto produtos[] = {
-                new Produto("Lamborghini", 1000000),
-                new Produto("Jipe", 46000),
-                new Produto("Brasília", 16000),
-                new Produto("Smart", 46000),
-                new Produto("Fusca", 17000)
-        };
+        try {
+            Produto produtos[] = {
+                    new Produto("Lamborghini", 1000000),
+                    new Produto("Jipe", 46000),
+                    new Produto("Brasília", 16000),
+                    new Produto("Smart", 46000),
+                    null,
+                    new Produto("Fusca", 17000)
+            };
 
-        int maisBarato = buscaMenor(produtos);
-        System.out.println(maisBarato);
-        System.out.println("O carro " + produtos[maisBarato].getNome()
-                + " é o mais barato, e custa "
-                + produtos[maisBarato].getPreco());
+            int maisBarato = buscaMenor(produtos);
+
+            System.out.println(maisBarato);
+            System.out.println("O carro " + produtos[maisBarato].getNome()
+                    + " é o mais barato, e custa "
+                    + produtos[maisBarato].getPreco());
+
+        } catch (NullPointerException exception) {
+            System.out.println("Seu array está vazio ");
+        }
     }
 }
+
